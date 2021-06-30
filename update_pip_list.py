349d46823435b6cb523425147b75dad9cd06098f
Mohammad <71057142+spacelover92@users.noreply.github.com>
2021-06-30 17:37:16 +0430

    bug fixed, log file improved
    re pattern updated for packages that have numbers in their name.

--- update_pip_list.py
+++ update_pip_list.py
@@ -8,14 +8,17 @@ os.system("pip list --outdated > pip_outdated_list.txt")
 with open("pip_outdated_list.txt") as file:
     outdated_list = file.read().splitlines()
 
-# remove temp file
-os.system("del pip_outdated_list.txt")
-
 # retrieve name of packages
 final_list = []
 for item in outdated_list[2:]:
-    final_list += re.findall(r"^[a-zA-Z-]*", item)
+    final_list += re.findall(r"^[a-zA-Z-_0-9]*", item)
 
 # update one by one
-for item in final_list:
-    os.system(f"python -m pip install --upgrade {item}")
+with open("pip_outdated_list.txt", "a") as file:
+    file.write("\nList of updated packages:\n")
+    for item in final_list:
+        file.write(item)
+        os.system(f"python -m pip install --upgrade {item}")
+
+# remove temp file
+# os.system("del pip_outdated_list.txt")
